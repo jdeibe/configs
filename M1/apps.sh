fbed7e8fe6a48b7d9981e570968aae08235d56a0
Jonas Deibe <jonas@hiotlabs.com>
2021-03-22 13:42:24 +0100

    M1 testing

---
+++ M1/apps.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Install MAS support
+brew install mas
+mas signin jonas.deibe@hotmail.com
+
+# Install from MAS
+mas install 443987910 # 1Password
+mas install 497799835 # xcode
+mas install 1488888843 # Nordvpn teams
+mas install 425424353 # The Unarchiver
+mas install 975937182 # Fantastical
+mas install 775737590 # iA Writer
+mas install 1176895641 # Spark
+mas install 1107421413 # 1Blocker
+mas install 409201541 # Pages
+mas install 411643860 # DaisyDisk
+
+# Install casks
+brew install 1password
+brew install spectacle
+#brew install adobe-creative-cloud
+#brew install brisync
+brew install cleanshot #make good gifs or screenshoots
+brew install dropbox
+brew install figma #mockup for apps
+brew install firefox
+brew install google-chrome
+brew install imagealpha
+brew install imageoptim
+brew install java
+brew install qlstephen #colors 2-monitor
+#brew install qsyncthingtray
+brew install slack
+#brew install sublime-text #text-editor
+#brew install tunnelblick #vpn
+#brew install xld #soundfiles
+
---
+++ M1/init.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Installs Homebrew, Git, git-extras, git-friendly, hub, Node.js, etc.
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Install XCode CLI tools
+xcode-select --install
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+# GNU core utilities (those that come with macOS are outdated)
+#brew install coreutils
+# GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+#brew install findutils
+
+# More recent versions of some macOS tools
+brew install grep
+
+# Install cask
+brew install cask
+
+# Java is required for some packages
+#brew tap caskroom/versions
+#brew cask install java
+
+# Git & friends
+brew install git
+brew install git-extras
+brew install git-lsf
+brew install gh
+brew install diff-so-fancy
+brew install tree
+
+# Syncthing
+brew install syncthing
+brew services start syncthing
+
+# Node & yarn & npm
+brew install node
+brew install yarn
+ibrew "Schniz/tap/fnm" # This cannot be installed under arm64 yet
+
+# NPM settings
+npm config set loglevel warn
+
+# Install QuickLook Plugins
+brew install qlcolorcode qlstephen qlmarkdown quicklook-json
+
+# Install fonts
+brew tap caskroom/fonts
+brew cask install font-fira-code
+
+# Python
+brew install python
+
+# Verify installation
+command -v git
+command -v fnm
+command -v node
+command -v npm
+command -v yarn
+command -v python
+
+# Remove outdated versions from the cellar
+brew cleanup
